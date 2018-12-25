dcee4ef10f42affe59d44c188fb13ef4fab4f2c7
chenhonzhou <chenhonzhou@gmail.com>
2018-12-26 00:44:29 +0800

    children z-r commit

---
+++ test/config.js
@@ -0,0 +1,25 @@
+var _configData = {
+  blog_title: "Kozo4のblog",
+  github_user: "lmk123",
+  github_repo: "blog",
+  pre_page: '10',
+  _DIY: {
+    favicon: "http://github.com/favicon.ico",
+    bg: "random",
+    headBg: "random",
+    slogan: "random",
+    footer_year: "2016",
+    footer_name: "陈大大哦了",
+    footer_link: "http://kozo4.ooo",
+    footer_keep: "京ICP我备你他妈的案",
+    mPlayer: "2110194986",
+    oldDate: "2016,09,10",
+    navTitle: "陈大大哦了",
+    slideBg: "https://i.loli.net/2018/09/20/5ba39df8950eb.gif",
+    avatar: "piku.svg",
+    name: 'kozo4',
+    desc: "普通人,伪前端狗",
+    // 雪花
+    Snow: false,
+  }
+}
