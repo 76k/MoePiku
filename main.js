de51190d4e94bba286ddeb6535a6294b3ea1b9b1
chenhonzhou <chenhonzhou@gmail.com>
2018-12-31 16:28:16 +0800

    eval all

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-// Author: kozo4
 // MoePiku v0.9.8 | MIT
 // Github: https://github.com/d1y/MoePiku
 // create by 2018 last commit for 2018//11/25
 // ----cat me----> http://kozo4.ooo
 
 Ractive.DEBUG = false;
-
+var postWrap = $('#post-content .container-fluid')
 function index(page) {
   // 传的参数转为整数,并不可能为0
   page = parseInt(page) || 1;
@@ -18,7 +17,7 @@ function index(page) {
   $('title').html(_configData['blog_title']);
   // 如果获取到了就生成内容,然后返回
   if (_G.postList[page] != undefined) {
-    $('#post-content .container-fluid').html(_G.postList[page]);
+    postWrap.html(_G.postList[page]);
     return;
   }
   // ajax请求
@@ -35,7 +34,7 @@ function index(page) {
     },
     beforeSend: function() {
       // 在数据未加载好之前的一个动画效果
-      $('#post-content .container-fluid').html('<img class="mx-auto" src="https://i.loli.net/2018/09/20/5ba3a331a8f3e.gif">');
+      postWrap.html('<div class="text-center pb-4 pt-4 mb-4 mt-4"><img class="mx-auto" src="https://i.loli.net/2018/09/20/5ba3a331a8f3e.gif"></div>');
     },
     success: function(data, textStatus, jqXHR) {
       // 返回的结果,其中data是数据
@@ -59,7 +58,7 @@ function index(page) {
         }
       });
       window._G.postList[page] = ractive.toHTML();
-      $('#post-content .container-fluid').html(window._G.postList[page]);
+      postWrap.html(window._G.postList[page]);
       //将文章列表的信息存到全局变量中，避免重复请求
       for (i in data) {
         var ractive = new Ractive({
@@ -83,7 +82,46 @@ function highlight() {
     hljs.highlightBlock(block);
   });
 }
-
+if(_configData['_DIY']['slogan']){
+  new Ractive({
+    el: "#user-header",
+    template: '#post-header',
+    data: {
+      slogan_con: _configData['_DIY']['slogan'],
+      slogan_form: _configData['_DIY']['slogan_form']
+    }
+  })
+}else{
+  $('body').append($('<script src="https://v1.hitokoto.cn/?encode=js&select=%23hitokoto" defer></script>'))
+  new Ractive({
+    el: "#user-header",
+    template: '#post-header',
+    data: {
+      slogan_con: _configData['_DIY']['slogan'],
+      slogan_form: _configData['_DIY']['slogan_form']
+    }
+  })
+}
+function playDate(e){
+  if(e){
+    return new Date().getFullYear()
+  }
+  var a = new Date() - new Date(_configData['_DIY']['oldDate']),
+      d = Math.floor(a / (24 * 3600 * 1000))
+      return d
+}
+new Ractive({
+  el: "#copyright",
+  template: "#footer",
+  data: {
+    footer_keep: _configData['_DIY']['footer_keep'],
+    footer_year: _configData['_DIY']['footer_year'],
+    footer_new_year: playDate(1),
+    footer_name: _configData['_DIY']['footer_name'],
+    playDate: playDate() + '天ヽ(✿ﾟ▽ﾟ)ノ',
+    footer_link: _configData['_DIY']['footer_link']
+  }
+})
 function detail(id) {
   if (!window._G) {
     window._G = {
@@ -94,13 +132,7 @@ function detail(id) {
   }
 
   if (_G.post[id].body != undefined) {
-    $('#post-content .container-fluid').html(_G.post[id].body);
-    // var tmp1 = $("#container img")
-    // for (var e = 0; e < tmp1.length; e++) {
-    //   new Zooming({
-    //     // options...
-    //   }).listen(tmp1[e])
-    // }
+    postWrap.html(_G.post[id].body);
     $('title').html(_G.post[id].title);
     highlight();
     return;
@@ -111,7 +143,7 @@ function detail(id) {
       // access_token: _config['access_token']
     },
     beforeSend: function() {
-      $('#post-content .container-fluid').html('<img src="https://i.loli.net/2018/09/20/5ba3a331a8f3e.gif">');
+      postWrap.html('<div class="text-center pb-4 pt-4 mb-4 mt-4"><img class="mx-auto" src="https://i.loli.net/2018/09/20/5ba3a331a8f3e.gif"></div>');
     },
     success: function(data) {
       var ractive = new Ractive({
@@ -125,7 +157,43 @@ function detail(id) {
       highlight();
     }
   });
-
+}
+$('#aboutMe').on('click',aboutMe)
+function aboutMe() {
+  $('.nav-bg').trigger("mousedown");
+  $.ajax({
+    url: "about.md",
+    beforeSend: function() {
+      postWrap.html('<div class="text-center pb-4 pt-4 mb-4 mt-4"><img class="mx-auto" src="https://i.loli.net/2018/09/20/5ba3a331a8f3e.gif"></div>');
+    },
+    success: function(data) {
+      postWrap.html(marked(data));
+      highlight();
+    }
+  })
+}
+function helpNote(){
+  var eNote = new Ractive({
+    template: '#note'
+  })
+  postWrap.html(eNote.toHTML())
+}
+$('#ph').on('click',ph)
+function ph() {
+  $('.nav-bg').trigger("mousedown");
+  $.ajax({
+    url: "photop.json",
+    beforeSend: function() {
+      postWrap.html('<div class="text-center pb-4 pt-4 mb-4 mt-4"><img class="mx-auto" src="https://i.loli.net/2018/09/20/5ba3a331a8f3e.gif"></div>');
+    },
+    success: function(data) {
+      postWrap.html('')
+      $(data).each(function(index,item) {
+        postWrap.get(0).innerHTML += '<img class="zooming" src="'+item+'">'
+      })
+      new Zooming({}).listen('.zooming')
+    }
+  })
 }
 var helpers = Ractive.defaults.data;
 helpers.markdown2HTML = function(content) {
@@ -138,97 +206,222 @@ var routes = {
   '/': index,
   'p:page': index,
   'post/:postId': detail,
+  'about': aboutMe,
+  'note': helpNote,
+  'ph': ph
 };
 var router = Router(routes);
 router.init('/');
 
-// ---------------
-//　返回顶部
-$(function() {
-  $('#back').hide(); //隐藏go to top按钮
-  $(window).scroll(function() {
-    // console.log($(this).scrollTop());
-    //当window的scrolltop距离大于1时，go to
-    if ($(this).scrollTop() > 100) {
-      $('#back').fadeIn();
-    } else {
-      $('#back').fadeOut();
-    }
-  });
-
-  $('#back a').click(function() {
-    $('html ,body').animate({
-      scrollTop: 0
-    }, 300);
-    return false;
-  });
+$('#up-back').click(function() {
+  $('html ,body').animate({
+    scrollTop: 0
+  }, 300);
+  return false;
 });
-
-function goPAGE() {
-  if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-    // console.log('手机端');
-  } else {
-    // console.log('pc端');
-  }
-}goPAGE();
-
-// var sf = new Snowflakes({
-//   color: "rgba(255,255,255,.5)",
-//   count: 80,
-//   minOpacity: 0.1,
-//   maxOpacity: 0.8
-// });
-// const ap = new APlayer({
-//   container: document.getElementById('aplayer'),
-//   fixed: true,
-//   audio: [{
-//     name: 'she goes',
-//     artist: 'atmozfears',
-//     url: 'https://files.catbox.moe/1efv4x.mp3',
-//     cover: 'https://i.loli.net/2018/12/22/5c1e0f7795f55.png'
-//   }]
-// });
+if(_configData['_DIY']['favicon'] == '' || _configData['_DIY']['favicon'] == ''){
+  $('link[rel=icon]').attr('href',"http://github.com/favicon.ico")
+}else{
+  $('link[rel=icon]').attr('href',_configData['_DIY']['favicon'])
+}
+if(_configData['_DIY']['headBg']){
+  $('#user-header').css({
+    background: 'url('+_configData['_DIY']['headBg']+')'
+  })
+}
+$('#navTitle').text(_configData['_DIY']['navTitle'])
+if(!_configData['_DIY']['slideBg']){
+  $('#mainNav .card-img-top').css({
+    "backgroundImage":"url('https://i.loli.net/2018/09/20/5ba39df8950eb.gif')"
+  })
+}else{
+  $('#mainNav .card-img-top').css({
+    "backgroundImage": "url("+_configData['_DIY']['slideBg']+")"
+  })
+}
+$('#avatar').attr('src',_configData['_DIY']['avatar'])
+$('#av-name').text(_configData['_DIY']['name'])
+$('.card-desc').text(_configData['_DIY']['desc'])
 $('#checkNav').on('click', function(event) {
-  if ($(this).attr('data-nav') === '0') {
-    $("#mainNav").css({
-      "transform": "translateX(0)"
-    })
+    $("#mainNav").addClass('active')
     $('.nav-bg').show()
     $('.nav-bg').on("mousedown", function(e) {
       // console.log(e);
-      $("#mainNav").css({
-        "transform": "translateX(-40rem)"
-      })
-      $('.nav-bg').hide()
+      $("#mainNav").removeClass('active')
+      $(this).hide()
     });
     event.stopPropagation();
     event.preventDefault()
-  }
 })
-if ($(window).scrollTop() != 0) {
-  setTimeout(function() {
-    $('.menu-bar').animate({
-      'top': '-10rem'
-    })
-  }, 400)
-}
-var p = 0,
-  t = 0;
+var p = 0,t = 0;
+$('#up-back').hide();
 $(window).on('scroll', function() {
-  p = $(this).scrollTop();
-  if (t < p) {
-    $('.menu-bar').css({
-      'top': '-10rem'
-    })
+  if ($(this).scrollTop() > 100) {
+    $('#up-back').fadeIn();
   } else {
-    $('.menu-bar').css({
-      'top': '0'
-    })
+    $('#up-back').fadeOut();
+  }
+  p = $(this).scrollTop();
+  if(p === 0){
+    $('.menu-bar').removeClass('active')
+    $('.menu-bar').removeClass('notactive')
+    return
+  }
+  if (t < p) {
+    $('.menu-bar').removeClass('active')
+    $('.menu-bar').addClass('notactive')
+  } else {
+    $('.menu-bar').removeClass('notactive')
+    $('.menu-bar').addClass('active')
   }
   setTimeout(function() {
     t = p;
   }, 0)
 })
+$(window).on('touchstart',function(e){
+  startX = e.touches[0].pageX;
+})
+$(window).on('touchend',function (e) {
+  endX = e.changedTouches[0].pageX;
+  if(endX - startX > 100){
+    $('#checkNav').trigger('click')
+  }
+})
+function active(ele) {
+  var n = ele.parent().next()
+  n.html(marked(n.html())).toggle("fast")
+}
+function goPAGE() {
+  if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+    // form Mobie
+  } else {
+    // form pc
+    var bgConfig = _configData['_DIY']['bg'],
+        musicConfig = _configData['_DIY']['mPlayer']
+    if(bgConfig == 'random'){
+      $('#bg').css({
+        "background-color":"rgba(0,0,0,.8)",
+        "background-image":"url('http://api.wuzuhua.cn/tp/api.php')",
+      })
+    }else{
+      $('#bg').css({
+        "background-image": 'url('+bgConfig+')'
+      })
+    }
+    if(musicConfig.constructor === Object){
+      new APlayer({
+        container: document.getElementById('aplayer'),
+        fixed: true,
+        audio: [{
+          name: musicConfig['name'],
+          artist: musicConfig['artist'],
+          url: musicConfig['url'],
+          cover: musicConfig['cover']
+        }]
+      });
+    }else if(typeof musicConfig == 'string'){
+      fetch163Playlist(musicConfig)
+        .then(function (data) {
+          new APlayer({
+            container: document.getElementById('aplayer'),
+            fixed: true,
+            audio: data
+          });
+        })
+        .catch(function (err) {
+          console.error(err);
+        })
+    }else if(musicConfig instanceof Array){
+      fetch163Songs(musicConfig)
+        .then(function (data) {
+          new APlayer({
+            container: document.getElementById('aplayer'),
+            fixed: true,
+            audio: data
+          });
+        })
+        .catch(function (err) {
+          console.error(err);
+        })
+    }
+    if(_configData['_DIY']['Snow']){
+      var sf = new Snowflakes({
+        color: "rgba(255,255,255,1)",
+        count: 80,
+        minOpacity: 0.1,
+        maxOpacity: 0.8
+      });
+    }
+  }
+}goPAGE();
+function fetch163Playlist(playlist_id) {
+  return new Promise(function (ok, err) {
+    fetch("https://v1.hitokoto.cn/nm/playlist/" + playlist_id)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        var arr = [];
+        data.playlist.tracks.map(function (value) {
+          arr.push(value.id);
+        });
+        return arr;
+      })
+      .then(function (ids) {
+        return fetch163Songs(ids);
+      })
+      .then(function (data) {
+        ok(data);
+      })
+      .catch(function (e) {
+        err(e);
+      });
+  })
+}
+function fetch163Songs(IDS) {
+  return new Promise(function (ok, err) {
+    var ids;
+    switch (typeof IDS) {
+      case 'number':
+        ids = [IDS];
+        break;
+      case 'object':
+        if (!Array.isArray(IDS)) {
+          err(new Error("Please enter array or number"));
+        }
+        ids = IDS;
+        break;
+      default:
+        err(new Error("Please enter array or number"));
+        break;
+    }
+    fetch("https://v1.hitokoto.cn/nm/summary/" + ids.join(",") + "?lyric=true&common=true")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        var songs = [];
+        data.songs.map(function (song) {
+          songs.push({
+            name: song.name,
+            url: song.url,
+            artist: song.artists.join("/"),
+            // album: song.album.name,
+            cover: song.album.picture,
+            lrc: song.lyric
+          });
+        });
+        return songs;
+      })
+      .then(function (result) {
+        ok(result);
+      })
+      .catch(function (e) {
+        err(e);
+      });
+  });
+}
+// version
 var Piku = {
   "Author": "Demi Kozo4",
   "Version": "0.8.9"
